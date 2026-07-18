2563fd07e2b4650d654e989409b0a2563ec43075
Awantika Amisha Sahai <sahaiawantika09@gmail.com>
2026-07-18 08:13:20 +0530

    fourth push

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const AuthRouter = require('./Routes/AuthRouter');
 const ProductsRouter = require('./Routes/ProductsRouter');
 
 require('dotenv').config();
-require('./Models/db');
+require('./models/db');
 const PORT = process.env.PORT || 8080;
 
 app.get('/ping', (req, res) =>{
